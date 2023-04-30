1a45b8fe799b85542fcff8739db792b00b7d56c4
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-04-30 19:15:46 +0600

    scroll restore added

--- src/common/layouts/AppLayout.tsx
+++ src/common/layouts/AppLayout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/layout/Navbar";
+import ScrollToTop from "../components/utils/ScrollToTop";
 
 const AppLayout = () => {
   return (
-    <>
+    <ScrollToTop>
       <Navbar />
       <Outlet />
-    </>
+    </ScrollToTop>
   );
 };
 
