c32906bd3f34a777f089be99e943486a2e2a8eae
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-08-22 15:26:33 +0600

    search background is now white

--- src/common/components/pages/home/HeroSearch.tsx
+++ src/common/components/pages/home/HeroSearch.tsx
@@ -6,13 +6,13 @@ const HeroSearch = () => {
     <Form
       action="/search"
       method="GET"
-      className="flex items-stretch gap-2 max-w-[600px] lg:w-[600px] bg-white/40 px-3 py-2 rounded hover:bg-white/50 duration-200 group"
+      className="flex items-stretch gap-2 max-w-[600px] lg:w-[600px] bg-background-content px-3 py-2 rounded duration-200 group"
     >
       <input
         required
         type="text"
         name="query"
-        className="py-0 px-2 focus:outline-none border-0 flex-1 text-white bg-transparent placeholder:text-background-base w-full group-hover:placeholder:text-background-content"
+        className="py-0 px-2 focus:outline-none border-0 flex-1 text-background-base placeholder:text-secondary-light bg-transparent w-full"
         placeholder="Search for movie or tv show"
       />
       <button type="submit" className="btn btn-primary">
